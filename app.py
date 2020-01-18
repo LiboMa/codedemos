cd12aae5cd8217f14a2a67a9ee46e6978a8cbe50
malibo8407@gmail.com <malibo8407@gmail.com>
2020-01-18 21:28:36 +0800

    add mock api

--- app.py
+++ app.py
@@ -2,16 +2,24 @@
 
 from flask import Flask
 app = Flask(__name__)
+import json
 import datetime as dt
 
 @app.route('/')
 def hello_world():
     message='''
-    <h2>Python-flask project. Running on openshift(ocp) </h2>
+    <h2>Python-flask project. Running on openshift(OKD env all-in-one) </h2>
     <p> //OS: CentOs 7, Openshift 3.11//</p>
     <p>{}</p>
     '''.format(dt.datetime.now())
-    return message 
+    return message
+
+@app.route('/api/mock')
+def test_api():
+
+    data = {"message":"Hi there, this app is running on ODK-openshift 3.11"}
+    return json.dumps(data)
+
 
 if __name__ == '__main__':
     app.run(debug=True)
