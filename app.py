122cc3362941ecab7cd9e43349f774a0945f628f
malibo8407@gmail.com <malibo8407@gmail.com>
2020-03-09 15:25:12 +0800

    add some module

--- app.py
+++ app.py
@@ -1,10 +1,12 @@
 #!/usr/bin/env python 
 
-from flask import Flask
+from flask import Flask, request, redirect, render_template, jsonify
+import time
 
 app = Flask(__name__)
 import json
 import datetime as dt
+import pprint
 
 
 @app.route('/')
@@ -23,6 +25,21 @@ def index():
 def test_api():
     data = {"message": "Hi there, this app is running on ODK-openshift 3.11"}
     return json.dumps(data)
+
+
+@app.route('/api/postdata', methods=('GET','POST'))
+def test_api_postdata():
+    if request.method == 'GET':
+        data = {"message": "Hi there, flask app, postdata api echo message."}
+        return json.dumps(data)
+    if request.method == 'POST':
+        data = request.json
+        print(pprint.pprint(dir(request)))
+        #response = {'timestamp':time.time(), 'message':data.decode('utf-8')}
+        response = {'timestamp':time.ctime(), 'message': data}
+        return  jsonify(response)
+    return 
+    
 
 
 if __name__ == '__main__':
