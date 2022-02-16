9792067a5a0cd0cdf03916198c7bac0ae80acc77
malibo8407@gmail.com <malibo8407@gmail.com>
2022-02-16 14:28:38 +0800

    update

--- app.py
+++ app.py
@@ -1,17 +1,47 @@
 #!/usr/bin/env python 
 
-from flask import Flask
+from flask import Flask, request, redirect, render_template, jsonify
+import time
+
 app = Flask(__name__)
+import json
 import datetime as dt
+import os
+import pprint
+
 
 @app.route('/')
-def hello_world():
-    message='''
-    <h2>Python-flask project. Running on openshift(ocp) </h2>
-    <p> //OS: CentOs 7, Openshift 3.11//</p>
+def index():
+    message = '''
+    <body>
+    <p><h2>Python-flask project V3. </h2></p>
+    <p>you are hitting: {} </p>
     <p>{}</p>
-    '''.format(dt.datetime.now())
-    return message 
+    <p></p></body>
+    '''.format(os.uname().nodename, dt.datetime.now())
+    return message
+
+
+@app.route('/api/mock')
+def test_api():
+    data = {"message": "Hi there, this app is running on k8s cluster"}
+    return json.dumps(data)
+
+
+@app.route('/api/postdata', methods=('GET','POST'))
+def test_api_postdata():
+    if request.method == 'GET':
+        data = {"message": "Hi there, flask app, postdata api echo message."}
+        return json.dumps(data)
+    if request.method == 'POST':
+        data = request.json
+        print(pprint.pprint(dir(request)))
+        #response = {'timestamp':time.time(), 'message':data.decode('utf-8')}
+        response = {'timestamp':time.ctime(), 'message': data}
+        return  jsonify(response)
+    return 
+    
+
 
 if __name__ == '__main__':
     app.run(debug=True)
