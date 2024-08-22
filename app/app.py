9c99c470c156b3f58d6eaeb17c3074c189d79337
Ma <malibo@603e5f8407f2.ant.amazon.com>
2024-08-22 21:08:13 +0800

    update version

--- app/app.py
+++ app/app.py
@@ -144,7 +144,7 @@ def index():
           <!-- <svg class="bd-placeholder-img card-img-top" width="100%" height="225" xmlns="http://www.w3.org/2000/svg" role="img" aria-label="Placeholder: Thumbnail" preserveAspectRatio="xMidYMid slice" focusable="false"><title>Placeholder</title><rect width="100%" height="100%" fill="#55595c"/><text x="50%" y="50%" fill="#eceeef" dy=".3em">Thumbnail</text></svg> -->
           <div class="card-body">
             <div class="container mt-4">
-              <p class="text-danger">Python-flask project <strong>v3Beta</strong></p>
+              <p class="text-danger">Python-flask project <strong>V4-BETA.</strong></p>
               <p>You are hitting: <strong>{}</strong> Running on AWS EKS/K8S application</p>
               <p><strong>Datetime:</strong> {}</p>
           </div>
